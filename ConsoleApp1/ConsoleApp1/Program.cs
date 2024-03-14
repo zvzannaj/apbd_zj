8745050110b7bd4ab2971e2f3a3ddb4bc5dc2435
zvzannaj <zuzannajaroszczyk2@gmail.com>
2024-03-14 11:35:29 +0100

    zadanie 4

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,5 +7,26 @@
     }
 
     return result / numbers.Length;
-} 
+}
+
+static int GetMax(int[] tab)
+{
+    if (tab.Length >= 1)
+    {
+
+        int max = tab[0];
+        if (tab.Length >= 1)
+            max = tab[0];
+        for (int i = 0; i < tab.Length; i++)
+        {
+            if (tab[i] > max)
+                max = tab[i];
+        }
+
+        return max;
+    }
+
+    return -1;
+}
+
 
